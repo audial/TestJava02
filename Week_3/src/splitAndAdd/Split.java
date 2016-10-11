9b9a73f66d38ec4f02c11552ecb62697591dff77
Michal Wlodarkiewicz <1991mmw@gmail.com>
2016-10-11 18:23:20 +0200

    Deleted text in Split class

--- Week_3/src/splitAndAdd/Split.java
+++ Week_3/src/splitAndAdd/Split.java
@@ -15,39 +15,8 @@ public class Split {
 
 	    for (int i = 0; i < numberString.length(); i++){
 	        num[i] = numberString.charAt(i) - '0';
-//	        System.out.println(num[i]);
 	        int sum = IntStream.of(num).sum();
 	        System.out.println(sum);
 	    }
-	    
-	    
-
-	    /*for (int i : num) {
-	        System.out.println(i);
-	    }*/
-		
-//		char[] chars = numberString.toCharArray();
-//		
-//		for(char characters : chars){
-			
-		
-			/*String newone = Integer.toString(characters);
-			System.out.println(newone);
-			int twone = Integer.parseInt(newone);
-			for(int i = 0 ; i > twone; i++){
-				twone += i;
-				System.out.println(twone);*/
-			}
-		}
-		
-		
-		
-		
-		
-		/*Scanner sc = new Scanner(System.in);
-		System.out.println("Enter some numbers");
-		String StringOfNumbers = sc.nextLine();
-		System.out.println(StringOfNumbers);
-		String[] SplitStrings = StringOfNumbers.split(":");
-	    System.out.println(SplitStrings);*/
-	
+	}
+}
